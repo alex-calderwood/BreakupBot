7f2e60563be7fbb42227f326d5fc50adf57e3e03
Alex <alexc@production.pro>
2018-07-31 11:34:46 -0400

    Added credentials.

---
+++ credentials.py
@@ -0,0 +1,6 @@
+import praw
+reddit = praw.Reddit(client_id='XXXX',
+                     client_secret='XXXX',
+                     password='XXXX',
+                     user_agent='testscript by XXXX',
+                     username='XXXX')
