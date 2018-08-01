d992f846f29f417a8fdf2ba6c77bab2a70314143
Alex <alexc@production.pro>
2018-08-01 14:18:38 -0400

    Added basic stat collection.

--- bot.py
+++ bot.py
@@ -4,6 +4,9 @@ import sqlite3
 import traceback
 import datetime
 import nltk
+import os
+import dill as pickle
+from collections import defaultdict
 
 # https://praw.readthedocs.io/en/latest/tutorials/reply_bot.html
 WAIT = 10  # time to wait before restarting (after exception)
@@ -39,6 +42,8 @@ class Database:
 
 class Bot:
 
+    stat_pickle = './.stats'
+
     def __init__(self, subreddit):
 
         print('Logging in...')
@@ -47,6 +52,8 @@ class Bot:
         print('Successfully logged in.')
 
         self.sub = subreddit
+
+        self.stats = PostStats(Bot.stat_pickle)
 
     def do(self):
         print('Checking /r/' + self.sub)
@@ -67,13 +74,66 @@ class Bot:
         print('Title:', submission.title)
         print('\t', submission.selftext.replace('\n', ' '))
         if self.is_interesting(submission):
-            print("AND IT'S INTERESTING", end="")
+            print("AND IT'S INTERESTING", end=" ")
 
         if cache:
             print('Caching', end="")
-            self.db.add(submission)
+            if not self.db.in_database(submission):
+                self.db.add(submission)
+                self.stats.update()
 
-        print('\n')
+        print()
+        self.stats.show()
+        self.stats.save()
+        print()
+
+
+class PostStats:
+
+    def __init__(self, file):
+        self.file = file
+        self.stats = self.load()
+
+    def load(self):
+        if os.path.isfile(self.file):
+            return pickle.load(open(self.file, 'rb'))
+        else:
+            return defaultdict(lambda: defaultdict(int))
+
+    def update(self):
+        now = datetime.datetime.now()
+        self.stats[now.day][now.hour] += 1
+
+    def show(self):
+        # now = datetime.datetime.now()
+        # hours = len(self.stats[now.day].keys())
+        # for l in range(3):
+        #     for i in range(hours + 2):
+        #         if i == 0 or i == hours + 1:
+        #             c = '|'
+        #         elif l == 2:
+        #             c = '-'
+        #         else:
+        #             c = ' '
+        #         print(c, end='')
+        #     print()
+
+        print('{} posts collected today, {} this hour'.format(self.posts_this_day(), self.posts_this_hour()))
+
+    def save(self):
+
+        if len(self.stats) == 0:
+            return
+
+        pickle.dump(self.stats, open(self.file, 'wb'))
+
+    def posts_this_day(self):
+        now = datetime.datetime.now()
+        return sum(count for (hour, count) in self.stats[now.day].items())
+
+    def posts_this_hour(self):
+        now = datetime.datetime.now()
+        return self.stats[now.day][now.hour]
 
 
 class BreakupBot(Bot):
@@ -89,7 +149,7 @@ class BreakupBot(Bot):
     #     return True in [word in submission.selftext.lower() for word in self.interesting]
 
     def process(self, submission, cache=True):
-        cache = cache and not self.db.in_database(submission) and self.is_interesting(submission)
+        cache = cache and self.is_interesting(submission)
         super(BreakupBot, self).process(submission, cache)
 
         tokenized = nltk.word_tokenize(submission.selftext)
